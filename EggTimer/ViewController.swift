b322cf31b10bf6c52cf0e3d5124b4611650ee346
Scott Doggett <sdoggett@lcs.on.ca>
2020-05-03 16:10:11 -0400

    timer says done after completed time

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,24 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTime = ["Soft": 300, "Medium": 420, "Hard": 720]
+    let eggTime = ["Soft": 3, "Medium": 4, "Hard": 7]
     
     var secondsRemaining = 60
+    
+    var timer = Timer()
 
-
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var progressBar: UIProgressView!
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
         
         let hardness = sender.currentTitle!
         
         secondsRemaining = eggTime[hardness]!
         
-        Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updatedTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updatedTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updatedTimer() {
         if secondsRemaining > 0 {
             print("\(secondsRemaining) seconds.")
             secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
         }
     }
         
